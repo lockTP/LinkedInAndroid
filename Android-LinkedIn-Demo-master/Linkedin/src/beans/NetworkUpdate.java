1d67b5f9593e82a734d02aca8174c4e80a355145
lxtlouise <xiaotingli@Xiaotings-MacBook-Pro.local>
2015-04-08 15:35:54 -0400

    NetworkUpdate
    NetworkUpdate

---
+++ Android-LinkedIn-Demo-master/Linkedin/src/beans/NetworkUpdate.java
@@ -0,0 +1,27 @@
+package beans;
+
+/**
+ * Created by xiaotingli on 4/8/15.
+ */
+public class NetworkUpdate {
+
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ Android-LinkedIn-Demo-master/Linkedin/src/linkedinAPI/NetworkUpdateAPI.java
@@ -0,0 +1,65 @@
+package linkedinAPI;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import beans.NetworkUpdate;
+import oauth.signpost.OAuthConsumer;
+import oauth.signpost.exception.OAuthCommunicationException;
+import oauth.signpost.exception.OAuthExpectationFailedException;
+import oauth.signpost.exception.OAuthMessageSignerException;
+
+/**
+ * Created by xiaotingli on 4/8/15.
+ */
+public class NetworkUpdateAPI {
+    public NetworkUpdate NetwordSearchAPI(OAuthConsumer consumer, String timeBefore, String timeAfter, String count, String keyword){
+        String st = null;
+        NetworkUpdate network = null;
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        HttpGet get = new HttpGet("https://api.linkedin.com/v1/people/~/network/updates");
+
+        try {
+            consumer.sign(get);
+        } catch (OAuthMessageSignerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (OAuthExpectationFailedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (OAuthCommunicationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } // here need the consumer for sign in for post the share
+        get.setHeader("content-type", "text/XML");
+
+        try {
+            st = EntityUtils.toString(httpclient.execute(get).getEntity(), "UTF-8");
+            JSONObject obj = new JSONObject(st);
+            network = new NetworkUpdate();
+            network.setTitle(obj.getString("firstName"));
+            network.setDescription(obj.getString("headline"));
+
+
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return network;
+    }
+}
